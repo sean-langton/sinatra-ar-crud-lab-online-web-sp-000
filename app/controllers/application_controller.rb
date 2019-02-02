e72b127895ef31a78a896e92d5d4a6932461d6e4
Sean Langton <slangton1@gmail.com>
2019-02-02 00:13:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/articles' do
-    @article = Article.new(title:params[:title], content:params[:content])
+    @article = Article.create(title:params[:title], content:params[:content])
     @articles = Article.all
     erb :index
   end
